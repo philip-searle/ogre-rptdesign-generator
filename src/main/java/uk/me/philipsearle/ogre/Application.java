32946927ed5b1e4c1abe207f3d17977ab223b258
Philip Searle <philip@philip-searle.me.uk>
2019-05-14 20:58:32 +0100

    Tidy-up rptdesign generator code
    Remove unnecessary master page creation method, commented-out code, and
    deprecated method useage.

--- src/main/java/uk/me/philipsearle/ogre/Application.java
+++ src/main/java/uk/me/philipsearle/ogre/Application.java
@@ -1,33 +1,20 @@
 package uk.me.philipsearle.ogre;
 
-import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.dom4j.DocumentException;
-import org.eclipse.birt.report.model.api.CellHandle;
-import org.eclipse.birt.report.model.api.ColumnHandle;
-import org.eclipse.birt.report.model.api.DataItemHandle;
+import org.eclipse.birt.report.model.api.DesignConfig;
 import org.eclipse.birt.report.model.api.DesignEngine;
 import org.eclipse.birt.report.model.api.DesignFileException;
 import org.eclipse.birt.report.model.api.ElementFactory;
-import org.eclipse.birt.report.model.api.GridHandle;
-import org.eclipse.birt.report.model.api.LabelHandle;
-import org.eclipse.birt.report.model.api.LibraryHandle;
 import org.eclipse.birt.report.model.api.OdaDataSetHandle;
 import org.eclipse.birt.report.model.api.OdaDataSourceHandle;
 import org.eclipse.birt.report.model.api.ReportDesignHandle;
-import org.eclipse.birt.report.model.api.RowHandle;
-import org.eclipse.birt.report.model.api.ScalarParameterHandle;
 import org.eclipse.birt.report.model.api.SessionHandle;
-import org.eclipse.birt.report.model.api.SharedStyleHandle;
 import org.eclipse.birt.report.model.api.SimpleMasterPageHandle;
-import org.eclipse.birt.report.model.api.StructureFactory;
-import org.eclipse.birt.report.model.api.TableHandle;
-import org.eclipse.birt.report.model.api.TextItemHandle;
 import org.eclipse.birt.report.model.api.activity.SemanticException;
-import org.eclipse.birt.report.model.api.elements.DesignChoiceConstants;
 
 import com.ibm.icu.util.ULocale;
 
@@ -41,10 +28,7 @@ class Application {
 	private static final String DATA_SET_NAME = "Data Set";
 
 	private ReportDesignHandle designHandle = null;
-	private LibraryHandle libraryHandle = null;
 	private ElementFactory designFactory = null;
-	private ElementFactory libraryFactory = null;
-	private StructureFactory structFactory = null;
 
 	public static void main(String[] args) throws SemanticException, IOException, DesignFileException, DocumentException {
 		new Application().buildReport();
@@ -56,18 +40,15 @@ class Application {
 			OgreXmlDataParser parser = new OgreXmlDataParser();
 			OgreField rootField = parser.extractFieldsFromOgreXml(is);
 
-			SessionHandle session = DesignEngine.newSession(ULocale.ENGLISH);
+			// Deprecated method, I know... 
+			//SessionHandle session = DesignEngine.newSession(ULocale.ENGLISH);
+			SessionHandle session = new DesignEngine(new DesignConfig()).newSessionHandle(ULocale.ENGLISH);
 			designHandle = session.createDesign();
-			libraryHandle = session.createLibrary();
 			designFactory = designHandle.getElementFactory();
-			libraryFactory = libraryHandle.getElementFactory();
-			structFactory = new StructureFactory();
 
 			buildMasterPages();
 			buildDataSource(xmlUrl);
 			buildDataSet("Data Set", rootField);
-			// buildParameter();
-			// buildBody();
 
 			designHandle.saveAs("D:\\Downloads\\Sample2.rptdesign");
 		}
@@ -104,40 +85,8 @@ class Application {
 
 	}
 
-	private void buildMasterPages() throws SemanticException, IOException {
-
-		SimpleMasterPageHandle masterpage = (SimpleMasterPageHandle) designFactory
-				.newSimpleMasterPage("Simple MasterPage");
-		// Grid in master page header
-		GridHandle grid = designFactory.newGridItem(null, 2, 1);
-		grid.setProperty("marginBottom", "1cm");
-		grid.setProperty("textAlign", "right");
-		grid.setProperty("verticalAlign", "baseline");
-		grid.setProperty("width", "100%");
-		ColumnHandle column0 = (ColumnHandle) grid.getColumns().get(0);
-		column0.setProperty("width", "4.26in");
-		ColumnHandle column1 = (ColumnHandle) grid.getColumns().get(1);
-		column1.setProperty("width", "1.625in");
-
-		CellHandle cell0 = grid.getCell(0, 0);
-		TextItemHandle text = designFactory.newTextItem("masterpage text");
-		text.setProperty("fontSize", "14pt");
-		text.setProperty("fontWeight", "bold");
-		text.setProperty("display", "block");
-		text.setProperty("contentType", "plain");
-		text.setContent("Created by: Actuate");
-		cell0.getContent().add(text);
-
-		// Text in master page footer
-		TextItemHandle text1 = designFactory.newTextItem("pageheader text");
-		text1.setContentType(DesignChoiceConstants.TEXT_CONTENT_TYPE_HTML);
-		text1.setContent("<value-of>new Date()</value-of>");
-
-		masterpage.getPageHeader().add(grid);
-		masterpage.getPageFooter().add(text1);
-
+	private void buildMasterPages() throws SemanticException {
+		SimpleMasterPageHandle masterpage = (SimpleMasterPageHandle) designFactory.newSimpleMasterPage("Simple MasterPage");
 		designHandle.getMasterPages().add(masterpage);
-
 	}
-
 }
